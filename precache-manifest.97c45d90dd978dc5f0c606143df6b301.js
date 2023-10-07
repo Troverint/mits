13f954fa24c66fda994d7a7cc758b6873678c05f
Troverint <thisspecial16@gmail.com>
2023-10-07 23:55:35 +0700

    Updates

---
+++ precache-manifest.97c45d90dd978dc5f0c606143df6b301.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8242b886c1a71e56cfac6210d7a7fe8",
+    "url": "/mits/index.html"
+  },
+  {
+    "revision": "db99f453d3b60271872b",
+    "url": "/mits/static/css/main.6f154879.chunk.css"
+  },
+  {
+    "revision": "59a0151e5e5721c51e8b",
+    "url": "/mits/static/js/2.91763de9.chunk.js"
+  },
+  {
+    "revision": "db99f453d3b60271872b",
+    "url": "/mits/static/js/main.399d6306.chunk.js"
+  },
+  {
+    "revision": "32be865820fdf389b8e9",
+    "url": "/mits/static/js/runtime~main.9191e808.js"
+  },
+  {
+    "revision": "08ee96139f4d15ece679cfcbc5726754",
+    "url": "/mits/static/media/bg.08ee9613.png"
+  },
+  {
+    "revision": "909707a3191744285edffc4097d07a36",
+    "url": "/mits/static/media/bg2.909707a3.png"
+  },
+  {
+    "revision": "ef2b354ad65b625cff252e44cefb9fae",
+    "url": "/mits/static/media/p1.ef2b354a.jpeg"
+  },
+  {
+    "revision": "610fafd7675534107ae8af39aa854831",
+    "url": "/mits/static/media/p2.610fafd7.jpeg"
+  },
+  {
+    "revision": "4ca20d75547bfe228fc403d8e5ffaa8e",
+    "url": "/mits/static/media/p3.4ca20d75.jpeg"
+  }
+]);
